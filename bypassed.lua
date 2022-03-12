21f91411c019e56a0dda5be38fabcc274e59c6b7
Didward <86981999+Didward@users.noreply.github.com>
2022-03-12 23:05:13 +0000

    bypassed.lua

---
+++ bypassed.lua
@@ -0,0 +1,5 @@
+-- Do not change anything!
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1e17/Luau-Misc/main/dark-hub-bypass/okbuddy.lua'))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RandomAdamYT/DarkHub/master/Init", true))()
